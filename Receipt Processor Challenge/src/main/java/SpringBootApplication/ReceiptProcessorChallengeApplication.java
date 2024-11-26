e6522a425c8559e7bd5289c67f51b202a7befd73
Lalith Cheruku <clalithadithya@gmail.com>
2024-11-26 16:38:20 -0700

    Create ReceiptProcessorChallengeApplication.java

---
+++ Receipt Processor Challenge/src/main/java/SpringBootApplication/ReceiptProcessorChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.receiptprocessor.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // initializing SpringBoot project for Receipt Processor Challenge
+public class ReceiptProcessorChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptProcessorChallengeApplication.class, args);
+	}
+
+}
